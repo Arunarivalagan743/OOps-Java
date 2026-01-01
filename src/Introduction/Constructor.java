fb2f65a8ca8ae100b5c801a914602e4be359c62b
Arunarivalagan743 <arunarivalagan774@gmail.com>
2026-01-01 09:41:04 +0530

    memory alloction of new keyword

--- src/Introduction/Constructor.java
+++ src/Introduction/Constructor.java
@@ -16,8 +16,13 @@ class Person
 }
 public class Constructor {
     public static void main(String[] args) {
-        Person p1 =  new Person();
+        Person p1 =  new Person(); //it creates a memory for the obejct for becuse new store complex data dynmaiclly stored in runtime
         System.out.println(p1.age);
         System.out.println(p1.name);
+
+        Student s3 =  new Student();
+        Student s4 =  s3;  //it just points to refrence of the s3 in heap.The s4 does not crets new mory and copy of s3
+        s3.name = "arunak";
+        System.out.println(s4.name);
     }
 }

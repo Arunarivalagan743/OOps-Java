40571ff2dadb60ff5cf8df87c133b3dbbf105c8a
Arunarivalagan743 <arunarivalagan774@gmail.com>
2026-01-01 11:03:52 +0530

    gc

---
+++ src/Introduction/Garbage.java
@@ -0,0 +1,35 @@
+package Introduction;
+
+class GarbageColl {
+    String name;
+
+    GarbageColl(String name) {
+        System.out.println("Object created");
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // This method MAY be called by the Garbage Collector
+        // It is NOT guaranteed to execute
+        // Memory is freed by the JVM after the object becomes unreachable
+        System.out.println("Object destroyed");
+    }
+}
+
+public class Garbage {
+    public static void main(String[] args) {
+
+        GarbageColl a1 = new GarbageColl("arun");
+
+        // Each iteration creates a new object
+        // The old object becomes unreachable and eligible for GC
+        for (int i = 0; i < 1_000_000_000; i++) {
+            a1 = new GarbageColl("hari");
+        }
+
+        // Program end
+        // Garbage Collection may or may not run before program exits
+        System.out.println("End of main");
+    }
+}
